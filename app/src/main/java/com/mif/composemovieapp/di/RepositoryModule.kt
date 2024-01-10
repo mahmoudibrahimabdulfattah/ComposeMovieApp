80a838135d95fe59291d22e6005ce7ea625e3e7a
dev7odaa <mah.khalil22@gmail.com>
2024-01-10 16:05:59 +0200

    [di] AppModule

---
+++ app/src/main/java/com/mif/composemovieapp/di/RepositoryModule.kt
@@ -0,0 +1,19 @@
+package com.mif.composemovieapp.di
+
+import com.mif.composemovieapp.movieList.data.repository.MovieListRepositoryImpl
+import com.mif.composemovieapp.movieList.domain.repository.MovieListRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+    @Binds
+    @Singleton
+    abstract fun bindMovieListRepository(
+        movieListRepositoryImpl: MovieListRepositoryImpl
+    ): MovieListRepository
+}
