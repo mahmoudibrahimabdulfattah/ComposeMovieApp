6aed36f80636b36f933d3dd0cb59be84856f1bff
dev7odaa <mah.khalil22@gmail.com>
2024-01-10 15:41:28 +0200

    [MovieListRepositoryImpl] logic of offline-first

--- app/src/main/java/com/mif/composemovieapp/movieList/data/remote/MovieApi.kt
+++ app/src/main/java/com/mif/composemovieapp/movieList/data/remote/MovieApi.kt
@@ -1,15 +1,16 @@
 package com.mif.composemovieapp.movieList.data.remote
 
 import com.mif.composemovieapp.BuildConfig
+import com.mif.composemovieapp.movieList.data.remote.respond.MovieListDto
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface MovieApi {
     @GET("movie/{category}")
-    suspend fun getMovieList(
+    suspend fun getMoviesList(
         @Path("category") category: String,
         @Query("page") page: Int,
         @Query("api_key") apiKey: String = BuildConfig.API_KEY
-    )
+    ): MovieListDto
 }
--- app/src/main/java/com/mif/composemovieapp/movieList/data/repository/MovieListRepositoryImpl.kt
+++ app/src/main/java/com/mif/composemovieapp/movieList/data/repository/MovieListRepositoryImpl.kt
@@ -1,27 +1,103 @@
 package com.mif.composemovieapp.movieList.data.repository
 
+import android.net.http.HttpException
+import android.os.Build
+import androidx.annotation.RequiresExtension
 import com.mif.composemovieapp.movieList.data.local.MovieDatabase
+import com.mif.composemovieapp.movieList.data.mapper.toMovie
+import com.mif.composemovieapp.movieList.data.mapper.toMovieEntity
 import com.mif.composemovieapp.movieList.data.remote.MovieApi
 import com.mif.composemovieapp.movieList.domain.model.Movie
 import com.mif.composemovieapp.movieList.domain.repository.MovieListRepository
 import com.mif.composemovieapp.movieList.util.Resource
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import java.io.IOException
 import javax.inject.Inject
 
 class MovieListRepositoryImpl @Inject constructor(
     private val movieApi: MovieApi,
     private val movieDatabase: MovieDatabase
 ): MovieListRepository {
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
     override suspend fun getMovieList(
         forceFetchFromRemote: Boolean,
         category: String,
         page: Int
     ): Flow<Resource<List<Movie>>> {
-        TODO("Not yet implemented")
+        return flow {
+
+            emit(Resource.Loading(true))
+
+            val localMovieList = movieDatabase.movieDao.getMovieListByCategory(category)
+
+            val shouldLoadLocalMovie = localMovieList.isNotEmpty() && !forceFetchFromRemote
+
+            if (shouldLoadLocalMovie) {
+                emit(Resource.Success(
+                    data = localMovieList.map { movieEntity ->
+                        movieEntity.toMovie(category)
+                    }
+                ))
+
+                emit(Resource.Loading(false))
+                return@flow
+            }
+
+            val movieListFromApi = try {
+                movieApi.getMoviesList(category, page)
+            } catch (e: IOException) {
+                e.printStackTrace()
+                emit(Resource.Error(message = "Error loading movies"))
+                return@flow
+            } catch (e: HttpException) {
+                e.printStackTrace()
+                emit(Resource.Error(message = "Error loading movies"))
+                return@flow
+            } catch (e: Exception) {
+                e.printStackTrace()
+                emit(Resource.Error(message = "Error loading movies"))
+                return@flow
+            }
+
+            val movieEntities = movieListFromApi.results.let {
+                it.map { movieDto ->
+                    movieDto.toMovieEntity(category)
+                }
+            }
+
+            movieDatabase.movieDao.upsertMovieList(movieEntities)
+
+            emit(Resource.Success(
+                movieEntities.map { it.toMovie(category) }
+            ))
+            emit(Resource.Loading(false))
+
+        }
     }
 
     override suspend fun getMovie(id: Int): Flow<Resource<Movie>> {
-        TODO("Not yet implemented")
+        return flow {
+
+            emit(Resource.Loading(true))
+
+            val movieEntity = movieDatabase.movieDao.getMovieById(id)
+
+            if (movieEntity != null) {
+                emit(
+                    Resource.Success(movieEntity.toMovie(movieEntity.category))
+                )
+
+                emit(Resource.Loading(false))
+                return@flow
+            }
+
+            emit(Resource.Error("Error no such movie"))
+
+            emit(Resource.Loading(false))
+
+
+        }
     }
 
 }
